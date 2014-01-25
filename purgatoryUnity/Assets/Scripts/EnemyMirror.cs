f9c694f6c98c85ed261ec48b658307bd97e3b6c0
awardell <adamwardell@gmail.com>
2014-01-25 15:35:37 -0800

    Added base class for enemy movement. Took some of enemy mirror's functionality and stuck it there

--- purgatoryUnity/Assets/Scripts/EnemyMirror.cs
+++ purgatoryUnity/Assets/Scripts/EnemyMirror.cs
@@ -1,30 +1,28 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class EnemyMirror : MonoBehaviour {
+public class EnemyMirror : EnemyMove {
 
-	public Transform player;
-	public bool mirrorX = false;
-	public bool mirrorY = false;
+	public bool mirrorX, mirrorY;
 	public Vector3 prevPos;
 
 	// Use this for initialization
 	void Start () {
-		player = GameObject.FindGameObjectWithTag ("Player").transform;
+		mirrorX = mirrorY = false;
 	}
-
+	
 	public void moveFromCollision() {
 		this.transform.position = prevPos;
 	}
 
 	// Update is called once per frame
-	protected abstract virtual void Update () {
+	public void Update () {
 		prevPos = this.transform.position;
 
 		this.rigidbody.velocity = new Vector3(
 			player.rigidbody.velocity.x * (mirrorX ? -1 : 1),
 			player.rigidbody.velocity.y * (mirrorY ? -1 : 1),
 			player.rigidbody.velocity.z
-			);
+		);
 	}
 }
---
+++ purgatoryUnity/Assets/Scripts/EnemyMove.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class EnemyMove : MonoBehaviour {
+
+	public Transform player;
+	
+	// Use this for initialization
+	void Awake () {
+		player = GameObject.FindGameObjectWithTag ("Player").transform;
+	}
+}
