e764bc25f148eb151d958b736d9d019a3966c982
Daron Lee <daronlee@usc.edu>
2014-01-25 21:51:41 -0800

    Refined foot creator.

--- purgatoryUnity/Assets/Scripts/FootCreator.cs
+++ purgatoryUnity/Assets/Scripts/FootCreator.cs
@@ -7,16 +7,30 @@ public class FootCreator : MonoBehaviour {
 	float spawnRate = 0.5f;
 	bool spawnLeft = true;
 	public float offset = -1.0f;
+	Vector3 lastSpawn;
+	public float spawnDistance = 0.50f;
 
 	// Use this for initialization
 	void Start () {
-	
+	lastSpawn = this.transform.position;
+	GameObject tempFoot;
+	Vector3 footLoc;
+	float angle = Vector3.Angle(Vector3.up, this.rigidbody2D.velocity);
+	if(this.rigidbody2D.velocity.x > 0)
+		angle *= -1;
+	Quaternion footRot = Quaternion.Euler(new Vector3(0, 0, angle));
+	footLoc = new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z + 1);
+	tempFoot = (GameObject)Instantiate(Resources.Load("Foot"), footLoc, footRot);
+	tempFoot = (GameObject)Instantiate(Resources.Load("Foot"), footLoc, footRot);
+	tempFoot.transform.localScale = new Vector3(tempFoot.transform.localScale.x * -1, tempFoot.transform.localScale.y, tempFoot.transform.localScale.z);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		spawnTimer += Time.deltaTime;
-		if(spawnTimer >= spawnRate) {
+//		spawnTimer += Time.deltaTime;
+//		if(spawnTimer >= spawnRate) {
+		if(Vector3.Distance(this.transform.position, lastSpawn) > spawnDistance) {
+			lastSpawn = this.transform.position;
 			GameObject tempFoot;
 			Vector3 footLoc;
 			float angle = Vector3.Angle(Vector3.up, this.rigidbody2D.velocity);
