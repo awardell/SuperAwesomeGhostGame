37b1c64ccf22c53c68369f80f1ffbabc45133b0a
Mike Lester <themikelester@gmail.com>
2014-01-26 11:16:44 -0800

    Merge branch 'master' of https://github.com/awardell/SuperAwesomeGhostGame
    Conflicts:
    	purgatoryUnity/Assets/Scenes/Level02.unity

---
+++ purgatoryUnity/Assets/LightSettings.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// Make the script also execute in edit mode.
+[ExecuteInEditMode()]  
+public class LightSettings : MonoBehaviour {
+	public Color ambientColor;
+
+	// Use this for initialization
+	void Start () {
+		RenderSettings.ambientLight = ambientColor;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
