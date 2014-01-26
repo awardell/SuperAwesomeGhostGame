81745c9c82dd365446637220e3d35afd09e2add5
awardell <adamwardell@gmail.com>
2014-01-26 13:57:28 -0800

    Between Scenes! With voiceovers!

---
+++ purgatoryUnity/Assets/Scripts/TransitionController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TransitionController : MonoBehaviour {
+
+	public string sceneName;
+	private AudioSource source;
+
+	void Awake () {
+		source = GetComponent<AudioSource> ();
+	}
+
+	// Use this for initialization
+	void Start () {
+		source.Play ();
+		Invoke ("LoadNextLevel", source.clip.length + 0.5f);
+	}
+
+	private void LoadNextLevel()
+	{
+		Application.LoadLevel (sceneName);
+	}
+}
